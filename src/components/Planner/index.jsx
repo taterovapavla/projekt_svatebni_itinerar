c48c9ee70678411a1d9282f51e104bbbc63dca0d
Kristýna Kopalová <kopalova30@gmail.com>
2023-12-02 11:55:04 +0100

    adding timeline

--- src/components/Planner/index.jsx
+++ src/components/Planner/index.jsx
@@ -1,12 +1,17 @@
-import './style.css'
+import './style.css';
+import { Chrono } from 'react-chrono';
+import { timelineInfo } from './timeline-info';
 
 export const Planner = () => {
+  console.log(timelineInfo);
   return (
     <>
       <div className="planner__header">
-        <h1 className='planner__heading'>Svatební online itinerář</h1>
+        <h1 className="planner__heading">Svatební online itinerář</h1>
       </div>
-      <div className='planner__content'></div>
+      <div className="planner__content">
+        <Chrono items={timelineInfo} mode="HORIZONTAL" nestedCardHeight={200} />
+      </div>
     </>
-  )
-}
+  );
+};
